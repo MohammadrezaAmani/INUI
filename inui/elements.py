f73222b5476853e750113c9c5d6a6431bdcc5b3c
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:54 +0330

    add I class

--- inui/elements.py
+++ inui/elements.py
@@ -42215,3 +42215,394 @@ class Img(BaseElement):
 
         
 
+
+class Input(BaseVoidElement):
+    """
+ 
+"""
+    def __init__(
+        self,
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            tagName="input",
+        )
+
+
+class Ins(BaseElement):
+    """
+The <ins> tag in HTML is used to specify a block of inserted text. The <ins> tag is typically used to mark a range of text that has been added to the document. The inserted text is rendered as underlined text by the web browsers although this property can be changed using CSS text-decoration property. The <ins> tag requires a starting and ending tag.  
+ 
+
+`Syntax:`
+
+
+
+<ins> Contents... </ins>
+
+`Attributes:` 
+
+* [`cite:`](https://www.geeksforgeeks.org/html-ins-cite-attribute/) It is used to specify the URL of the document or message which denotes the reason of inserting the text.
+* [`datetime:`](https://www.geeksforgeeks.org/html-ins-datetime-attribute/) It is used to specify the date and time of the inserted text. The datetime is inserted in the format *YYYY-MM-DDThh:mm:ssTZD*.
+
+`Example 1:` This example describe the use of <ins> tag. 
+
+
+```html
+<!DOCTYPE html><html> <body><h1>GeeksforGeeks</h1> <h2>HTML ins Tag</h2>  <p>GeeksforGeeks is a <del>mathematical</del><ins>computer</ins> science portal</p> </body> </html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210205115055/ins1.PNG)
+
+`Example 2:` This example uses the <ins> tag with the datetime attribute and also use some CSS styles.   
+ 
+
+
+```html
+<!DOCTYPE html><html> <head><style>del {color: red;} ins {color: green;}</style></head> <body><h1>GeeksforGeeks</h1> <h2>HTML ins Tag</h2>  <p>GeeksforGeeks is a <del>mathematical</del><ins datetime="2018-11-21T15:55:03Z">computer</ins> science portal</p> </body> </html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210205115426/ins2.PNG)
+
+`Supported Browsers:` 
+
+* Google Chrome
+* Edge 12
+* Internet Explorer
+* Firefox 1
+* Opera
+* Safari
+
+  
+The <ins> tag in HTML is used to specify a block of inserted text. The <ins> tag is typically used to mark a range of text that has been added to the document. The inserted text is rendered as underlined text by the web browsers although this property can be changed using CSS text-decoration property. The <ins> tag requires a starting and ending tag.  
+ 
+
+`Syntax:`
+
+
+
+<ins> Contents... </ins>
+
+`Attributes:` 
+
+* [`cite:`](https://www.geeksforgeeks.org/html-ins-cite-attribute/) It is used to specify the URL of the document or message which denotes the reason of inserting the text.
+* [`datetime:`](https://www.geeksforgeeks.org/html-ins-datetime-attribute/) It is used to specify the date and time of the inserted text. The datetime is inserted in the format *YYYY-MM-DDThh:mm:ssTZD*.
+
+`Example 1:` This example describe the use of <ins> tag. 
+
+
+```html
+<!DOCTYPE html><html> <body><h1>GeeksforGeeks</h1> <h2>HTML ins Tag</h2>  <p>GeeksforGeeks is a <del>mathematical</del><ins>computer</ins> science portal</p> </body> </html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210205115055/ins1.PNG)
+
+`Example 2:` This example uses the <ins> tag with the datetime attribute and also use some CSS styles.   
+ 
+
+
+```html
+<!DOCTYPE html><html> <head><style>del {color: red;} ins {color: green;}</style></head> <body><h1>GeeksforGeeks</h1> <h2>HTML ins Tag</h2>  <p>GeeksforGeeks is a <del>mathematical</del><ins datetime="2018-11-21T15:55:03Z">computer</ins> science portal</p> </body> </html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210205115426/ins2.PNG)
+
+`Supported Browsers:` 
+
+* Google Chrome
+* Edge 12
+* Internet Explorer
+* Firefox 1
+* Opera
+* Safari
+
+  
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="ins",
+        )
+
+        
+
