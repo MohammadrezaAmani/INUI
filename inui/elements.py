bd61ffa5f333476d07508b789b5ac0609eb6e49d
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:56 +0330

    add Style class

--- inui/elements.py
+++ inui/elements.py
@@ -51073,3 +51073,234 @@ class Span(BaseElement):
 
         
 
+
+class Strike(BaseElement):
+    """
+In this article, we will know the `HTML <strike> tag`, along with understanding its implementation through the example. The <strike> tag defines a strike or line through Text.  This tag creates a cut line in the text. This tag is depreciated from HTML 5. Now, the [<del> tag](https://www.geeksforgeeks.org/html-del-tag/#:~:text=The%20tag%20in%20HTML,a%20starting%20and%20ending%20tag.) is used instead of this tag.
+
+`Syntax:`
+
+
+
+<strike> Contents </strike>
+
+`Note:` This tag is not supported in HTML5, instead of this tag, we can use [HTML del Tag](https://www.geeksforgeeks.org/html-del-tag/) or [HTML ins Tag](https://www.geeksforgeeks.org/html-ins-tag/) or use CSS [text-decoration](https://www.geeksforgeeks.org/css-text-decoration-property/#:~:text=The%20text%2Ddecoration%20property%20is,and%20text%2Ddecoration%2Dstyle.) property.
+
+`Example:` In this example, we simply use strike tag on text `Hi Geeks!`
+
+```html
+<!DOCTYPE html><html> <body> <!-- Strike Tag --><h2>Welcome To GeeksforGeeks</h2><strike>Hi Geeks!</strike></body> </html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210920130309/2010.png)HTML <strike> tag
+
+`Example:` Below example illustrates the <strike> tag in HTML.
+
+```html
+<!DOCTYPE html><html> <body><h1>Welcome to GeeksforGeeks</h1><h2>strike Tag</h2> <!-- Html strike tag --><strike>GeeksforGeeks</strike></body> </html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210920115346/209.png)HTML <strike> Tag
+
+`Supported Browsers:`
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+* Microsoft Edge 12 and above
+
+  
+In this article, we will know the `HTML <strike> tag`, along with understanding its implementation through the example. The <strike> tag defines a strike or line through Text.  This tag creates a cut line in the text. This tag is depreciated from HTML 5. Now, the [<del> tag](https://www.geeksforgeeks.org/html-del-tag/#:~:text=The%20tag%20in%20HTML,a%20starting%20and%20ending%20tag.) is used instead of this tag.
+
+`Syntax:`
+
+
+
+<strike> Contents </strike>
+
+`Note:` This tag is not supported in HTML5, instead of this tag, we can use [HTML del Tag](https://www.geeksforgeeks.org/html-del-tag/) or [HTML ins Tag](https://www.geeksforgeeks.org/html-ins-tag/) or use CSS [text-decoration](https://www.geeksforgeeks.org/css-text-decoration-property/#:~:text=The%20text%2Ddecoration%20property%20is,and%20text%2Ddecoration%2Dstyle.) property.
+
+`Example:` In this example, we simply use strike tag on text `Hi Geeks!`
+
+```html
+<!DOCTYPE html><html> <body> <!-- Strike Tag --><h2>Welcome To GeeksforGeeks</h2><strike>Hi Geeks!</strike></body> </html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210920130309/2010.png)HTML <strike> tag
+
+`Example:` Below example illustrates the <strike> tag in HTML.
+
+```html
+<!DOCTYPE html><html> <body><h1>Welcome to GeeksforGeeks</h1><h2>strike Tag</h2> <!-- Html strike tag --><strike>GeeksforGeeks</strike></body> </html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210920115346/209.png)HTML <strike> Tag
+
+`Supported Browsers:`
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+* Microsoft Edge 12 and above
+
+  
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="strike",
+        )
+
+        
+
