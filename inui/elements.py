e6794a93379c517d8710b813e8ce8a829137f786
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:52 +0330

    add Big class

--- inui/elements.py
+++ inui/elements.py
@@ -32046,3 +32046,224 @@ ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksfo
 
         
 
+
+class Bdo(BaseElement):
+    """
+The <bdo> stands for Bi-Directional Override. This tag is used to specify the text direction or used to change the current direction.
+
+`Syntax:`   
+ 
+
+
+
+<bdo dir> Contents... </bdo>
+
+`Attributes:` This element contains dir attributes which are used to specify the direction of text written inside the <bdo> element. The [dir attribute](https://www.geeksforgeeks.org/html-dir-attribute/) contains two values which are listed below:   
+ 
+
+* `rtl:` The text direction from right to left (reverse the text).
+* `ltr:` The text direction from left to right.
+
+`Example:` The below example illustrate the bdo tag.  
+ 
+
+```html
+<!DOCTYPE html><html> <body><h1>GeeksforGeeks</h1><h2><bdo> Tag</h2><!--This is bdo ltr Tag--><bdo dir="ltr">GeeksforGeeks</bdo><br><!--This is bdo rtl Tag--><bdo dir="rtl">GeeksforGeeks</bdo></body> </html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210205172022/bd.png)
+
+`Supported Browsers:`
+
+* Apple Safari
+* Edge 12 and above
+* Google Chrome
+* Firefox
+* Opera
+* Internet Explorer
+The <bdo> stands for Bi-Directional Override. This tag is used to specify the text direction or used to change the current direction.
+
+`Syntax:`   
+ 
+
+
+
+<bdo dir> Contents... </bdo>
+
+`Attributes:` This element contains dir attributes which are used to specify the direction of text written inside the <bdo> element. The [dir attribute](https://www.geeksforgeeks.org/html-dir-attribute/) contains two values which are listed below:   
+ 
+
+* `rtl:` The text direction from right to left (reverse the text).
+* `ltr:` The text direction from left to right.
+
+`Example:` The below example illustrate the bdo tag.  
+ 
+
+```html
+<!DOCTYPE html><html> <body><h1>GeeksforGeeks</h1><h2><bdo> Tag</h2><!--This is bdo ltr Tag--><bdo dir="ltr">GeeksforGeeks</bdo><br><!--This is bdo rtl Tag--><bdo dir="rtl">GeeksforGeeks</bdo></body> </html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210205172022/bd.png)
+
+`Supported Browsers:`
+
+* Apple Safari
+* Edge 12 and above
+* Google Chrome
+* Firefox
+* Opera
+* Internet Explorer
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="bdo",
+        )
+
+        
+
