d9e6e2293c347b58a2e440b64d63ad8fcc52f2cc
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 23:25:57 +0330

    update to version 0.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 # This gets deployed when a new release is made by github actions
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # CHANGEME VARS
 PACKAGE_NAME = "inui"
