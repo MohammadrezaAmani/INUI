69800f35e017b2b71ea10ba2bd1e9c3a13627e94
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:56 +0330

    add Span class

--- inui/elements.py
+++ inui/elements.py
@@ -50527,3 +50527,258 @@ ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksfo
 
         
 
+
+class Source(BaseElement):
+    """
+The <source> tag in HTML is used to attach multimedia files like audio, video and pictures. The <audio>, <video> and <picture> elements contain the <source> element. 
+
+`Syntax:`
+
+
+
+<source src="" type=""> 
+    // Statements
+</source>
+
+`Attributes:` This tag contains five attributes as mentioned above and described below:
+
+* [`src`](https://www.geeksforgeeks.org/html-source-srcset-attribute/?ref=rp)`:` It is used to hold the path of media content.
+* `media:` It is used to define the type of the media content.
+* [`srcset`](https://www.geeksforgeeks.org/html-source-srcset-attribute/?ref=rp)`:` It is used to specify the URL of image used in different situations.
+* `sizes:` It is used to specify the sizes of image in different page layout.
+* `type:` It is used to specify the MIME-type resource.
+
+Below examples illustrate the <source> tag in HTML: 
+
+`Example 1:` This example uses <source> tag with the video media file. 
+
+
+```html
+<!DOCTYPE html><html> <head><title>HTML source Tag</title></head> <body><h1 style="color:green;">GeeksforGeeks</h1> <h2>HTML &lt;source&gt; Tag</h2> <video width="400" height="350" controls><source src="video.mp4" type="video/mp4"><source src="video.ogg" type="video/ogg"></video></body> </html> |
+
+```
+`Output:`
+
+ ![](https://media.geeksforgeeks.org/wp-content/uploads/20190821121240/source11.png) 
+
+`Example 2:` This example uses <source> tag with the audio media files. 
+
+
+```html
+<!DOCTYPE html><html> <head><title>HTML source Tag</title></head> <body><h1 style="color:green;">GeeksforGeeks</h1> <h2>HTML &lt;source&gt; Tag</h2> <audio controls><source src="audio.mp3" type="audio/mp3"></audio></body> </html> |
+
+```
+`Output:`
+
+ ![](https://media.geeksforgeeks.org/wp-content/uploads/20190821121242/source21.png) 
+
+`Supported Browsers:` The browsers supported by `HTML <source> Tag` are listed below:
+
+* Google Chrome
+* Edge 12
+* Internet Explorer 9.0
+* Firefox 3.5
+* Safari
+* Opera
+
+  
+The <source> tag in HTML is used to attach multimedia files like audio, video and pictures. The <audio>, <video> and <picture> elements contain the <source> element. 
+
+`Syntax:`
+
+
+
+<source src="" type=""> 
+    // Statements
+</source>
+
+`Attributes:` This tag contains five attributes as mentioned above and described below:
+
+* [`src`](https://www.geeksforgeeks.org/html-source-srcset-attribute/?ref=rp)`:` It is used to hold the path of media content.
+* `media:` It is used to define the type of the media content.
+* [`srcset`](https://www.geeksforgeeks.org/html-source-srcset-attribute/?ref=rp)`:` It is used to specify the URL of image used in different situations.
+* `sizes:` It is used to specify the sizes of image in different page layout.
+* `type:` It is used to specify the MIME-type resource.
+
+Below examples illustrate the <source> tag in HTML: 
+
+`Example 1:` This example uses <source> tag with the video media file. 
+
+
+```html
+<!DOCTYPE html><html> <head><title>HTML source Tag</title></head> <body><h1 style="color:green;">GeeksforGeeks</h1> <h2>HTML &lt;source&gt; Tag</h2> <video width="400" height="350" controls><source src="video.mp4" type="video/mp4"><source src="video.ogg" type="video/ogg"></video></body> </html> |
+
+```
+`Output:`
+
+ ![](https://media.geeksforgeeks.org/wp-content/uploads/20190821121240/source11.png) 
+
+`Example 2:` This example uses <source> tag with the audio media files. 
+
+
+```html
+<!DOCTYPE html><html> <head><title>HTML source Tag</title></head> <body><h1 style="color:green;">GeeksforGeeks</h1> <h2>HTML &lt;source&gt; Tag</h2> <audio controls><source src="audio.mp3" type="audio/mp3"></audio></body> </html> |
+
+```
+`Output:`
+
+ ![](https://media.geeksforgeeks.org/wp-content/uploads/20190821121242/source21.png) 
+
+`Supported Browsers:` The browsers supported by `HTML <source> Tag` are listed below:
+
+* Google Chrome
+* Edge 12
+* Internet Explorer 9.0
+* Firefox 3.5
+* Safari
+* Opera
+
+  
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="source",
+        )
+
+        
+
