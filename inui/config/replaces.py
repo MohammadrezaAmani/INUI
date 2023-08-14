1677835027fdf553b284ca749dc19d0a8f7c031d
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 15:22:03 +0330

    add replace attributes file

---
+++ inui/config/replaces.py
@@ -0,0 +1,11 @@
+replace = {
+    "classs": "class",
+    "forr": "for",
+    "accessskey": "accesskey",
+    "aria_lable": "aria-label",
+    "comment": "!--",
+    "aria_describedby": "aria-describedby",
+    "iss": "is",
+    "doctype": "!DOCTYPE HTML",
+    "typee": "type",
+}
