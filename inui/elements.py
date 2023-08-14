392ed862c65de5d75bfc35d692c7d3d43f007b11
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:53 +0330

    add Dl class

--- inui/elements.py
+++ inui/elements.py
@@ -37196,3 +37196,240 @@ ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksfo
 
         
 
+
+class Dir(BaseElement):
+    """
+It is used to make a list of directory titles. It is not supported in HTML 5 <ul> or CSS are used instead of <dir> tag.
+
+`Syntax:` 
+
+
+
+<dir> Lists... </dir>
+
+Below example illustrates the <dir> tag in HTML:  
+`Example:` 
+
+```html
+<!DOCTYPE html><html><body> <h1>GeeksforGeeks</h1><h2><dir> Tag</h2> <p>List of all computer science subjects:</p> <!-- dir tag starts here--><dir><li>Operating system</li><li>Data Structures</li><li>computer network</li><li>Dbms</li><li>Oops</li></dir><!-- dir tag ends here--> </body></html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210702114451/dir.png)
+
+`Optional Attributes:` This tag contains single attributes *Compact* which is optional. It is used to specify the list size render smaller than normal by reducing the space between list items. It is a boolean attribute.  
+`Example:` 
+
+```html
+<!DOCTYPE html><html><head><title>dir tag</title><style>.gfg {font-size:40px;font-weight:bold;color:green;}.geeks {font-size:25px;font-weight:bold;}</style></head><body><div class = "gfg">GeeksforGeeks</div><div class = "geeks"><dir> Tag</div> <p>List of all computer science subjects:</p> <dir compact><li>Operating system</li><li>Data Structures</li><li>computer network</li><li>Dbms</li><li>Oops</li></dir></body></html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/dir-1.png)
+
+`Note:` The compact attribute is not supported by any browser.
+
+`Supported Browsers:` 
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+
+  
+It is used to make a list of directory titles. It is not supported in HTML 5 <ul> or CSS are used instead of <dir> tag.
+
+`Syntax:` 
+
+
+
+<dir> Lists... </dir>
+
+Below example illustrates the <dir> tag in HTML:  
+`Example:` 
+
+```html
+<!DOCTYPE html><html><body> <h1>GeeksforGeeks</h1><h2><dir> Tag</h2> <p>List of all computer science subjects:</p> <!-- dir tag starts here--><dir><li>Operating system</li><li>Data Structures</li><li>computer network</li><li>Dbms</li><li>Oops</li></dir><!-- dir tag ends here--> </body></html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210702114451/dir.png)
+
+`Optional Attributes:` This tag contains single attributes *Compact* which is optional. It is used to specify the list size render smaller than normal by reducing the space between list items. It is a boolean attribute.  
+`Example:` 
+
+```html
+<!DOCTYPE html><html><head><title>dir tag</title><style>.gfg {font-size:40px;font-weight:bold;color:green;}.geeks {font-size:25px;font-weight:bold;}</style></head><body><div class = "gfg">GeeksforGeeks</div><div class = "geeks"><dir> Tag</div> <p>List of all computer science subjects:</p> <dir compact><li>Operating system</li><li>Data Structures</li><li>computer network</li><li>Dbms</li><li>Oops</li></dir></body></html> |
+
+```
+`Output:`   
+ 
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/dir-1.png)
+
+`Note:` The compact attribute is not supported by any browser.
+
+`Supported Browsers:` 
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+
+  
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="dir",
+        )
+
+        
+
