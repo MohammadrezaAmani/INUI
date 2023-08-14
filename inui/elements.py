3c92693ea04a146be7be84298826f325011bcb70
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:55 +0330

    add Object class

--- inui/elements.py
+++ inui/elements.py
@@ -46052,3 +46052,212 @@ ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksfo
 
         
 
+
+class Noembed(BaseElement):
+    """
+The `<noembed>` tag is used to show that the browsers is not supported by `<embed>` tag. This `<noembed>` tag will inform the user what is missing in the users browsers. 
+
+`Note: <noembed>` tag is not supported in html5.
+
+`Syntax:`
+
+
+
+<noembed> Element </noembed>
+
+`Example:` 
+
+
+```html
+<!DOCTYPE html><html> <head><title>embed Tag</title><style>q {color: #00cc00;font-style: italic;}</style></head> <body><center><br><embed src="<https://media.geeksforgeeks.org/wp-content/uploads/20190825000042/geeks-221.png">><noembed><img src="<https://media.geeksforgeeks.org/wp-content/uploads/20190821123122/gfgsm.png">alt="Alternative Media"></noembed></embed></center></body> </html> |
+
+```
+`Output:`
+
+* IF browsers supported by `<embed>`: ![](https://media.geeksforgeeks.org/wp-content/uploads/20190825000042/geeks-221.png)
+* IF browsers not supported by `<embed>` then `<noembed>` activate: ![](https://media.geeksforgeeks.org/wp-content/uploads/20190821123122/gfgsm.png)
+
+`Supported Browsers`: The browser supported by `<noembed>` tag are listed below:
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Safari
+* Opera
+The `<noembed>` tag is used to show that the browsers is not supported by `<embed>` tag. This `<noembed>` tag will inform the user what is missing in the users browsers. 
+
+`Note: <noembed>` tag is not supported in html5.
+
+`Syntax:`
+
+
+
+<noembed> Element </noembed>
+
+`Example:` 
+
+
+```html
+<!DOCTYPE html><html> <head><title>embed Tag</title><style>q {color: #00cc00;font-style: italic;}</style></head> <body><center><br><embed src="<https://media.geeksforgeeks.org/wp-content/uploads/20190825000042/geeks-221.png">><noembed><img src="<https://media.geeksforgeeks.org/wp-content/uploads/20190821123122/gfgsm.png">alt="Alternative Media"></noembed></embed></center></body> </html> |
+
+```
+`Output:`
+
+* IF browsers supported by `<embed>`: ![](https://media.geeksforgeeks.org/wp-content/uploads/20190825000042/geeks-221.png)
+* IF browsers not supported by `<embed>` then `<noembed>` activate: ![](https://media.geeksforgeeks.org/wp-content/uploads/20190821123122/gfgsm.png)
+
+`Supported Browsers`: The browser supported by `<noembed>` tag are listed below:
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Safari
+* Opera
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="noembed",
+        )
+
+        
+
