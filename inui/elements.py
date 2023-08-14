c1125f4d044bae70b2cd9829ecd331a648f1a67d
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:51 +0330

    add A class

--- inui/elements.py
+++ inui/elements.py
@@ -29345,3 +29345,214 @@ ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksfo
 
         
 
+
+class Acronym(BaseElement):
+    """
+The <acronym> tag in HTML is used to define the acronym. The <acronym> tag is used to spell out another word. It is used to give useful information to browsers, translation systems, and search-engines. This tag is not supported in HTML 5 otherwise use [<abbr>](https://geeksforgeeks.org/html-abbr-tag/#:~:text=The%20tag(Abbreviation,systems%2C%20and%20search-engines.) Tag.
+
+`Syntax:` 
+
+
+
+<acronym title=""> Short Form </acronym>
+
+`Attribute:` This tag accepts an optional attribute as mentioned above and described below:
+
+* [`title`](https://www.geeksforgeeks.org/html-title-attribute/):Itis used to specify extra information about the element. When the mouse moves over the element then it shows the information.
+
+`Example:` 
+
+
+```html
+<!DOCTYPE html><html> <body><h1>GeeksforGeeks</h1><h2>This is HTML acronym Tag</h2><acronym title="GeeksforGeeks">GFG</acronym><br><acronym title="Operating System">OS</acronym></body> </html> |
+
+```
+`Output:` 
+
+![Screenshot-from-2023-06-30-16-34-32.png](https://media.geeksforgeeks.org/wp-content/uploads/20230630163728/Screenshot-from-2023-06-30-16-34-32-300.png)acronym Tag
+
+`Supported Browser:` 
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+The <acronym> tag in HTML is used to define the acronym. The <acronym> tag is used to spell out another word. It is used to give useful information to browsers, translation systems, and search-engines. This tag is not supported in HTML 5 otherwise use [<abbr>](https://geeksforgeeks.org/html-abbr-tag/#:~:text=The%20tag(Abbreviation,systems%2C%20and%20search-engines.) Tag.
+
+`Syntax:` 
+
+
+
+<acronym title=""> Short Form </acronym>
+
+`Attribute:` This tag accepts an optional attribute as mentioned above and described below:
+
+* [`title`](https://www.geeksforgeeks.org/html-title-attribute/):Itis used to specify extra information about the element. When the mouse moves over the element then it shows the information.
+
+`Example:` 
+
+
+```html
+<!DOCTYPE html><html> <body><h1>GeeksforGeeks</h1><h2>This is HTML acronym Tag</h2><acronym title="GeeksforGeeks">GFG</acronym><br><acronym title="Operating System">OS</acronym></body> </html> |
+
+```
+`Output:` 
+
+![Screenshot-from-2023-06-30-16-34-32.png](https://media.geeksforgeeks.org/wp-content/uploads/20230630163728/Screenshot-from-2023-06-30-16-34-32-300.png)acronym Tag
+
+`Supported Browser:` 
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="acronym",
+        )
+
+        
+
