6847266bcab474f72d9a73debbf6f40a1c36c4a3
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:55 +0330

    add Noscript class

--- inui/elements.py
+++ inui/elements.py
@@ -45841,3 +45841,214 @@ ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksfo
 
         
 
+
+class Nobr(BaseElement):
+    """
+The `<nobr>` tag is used to create a single line text, that does not matter how long the statement is, this tag used with [`<wbr>`](https://www.geeksforgeeks.org/html-5-wbr-tag/) tag. The created single line statement bring the horizontal scroll to read the whole line. The `<nobr>` tag is exact opposite of [<br>](https://www.geeksforgeeks.org/html-br-tag/) tag. You can use the CSS white space property as a replacement of this tag. 
+
+`Note:` <nobr> tag is not supported in html5.
+
+`Syntax:`
+
+
+
+<nobr> Statement </nobr>
+
+`Attribute:` This tag doesn’t contain any attribute. Below example illustrates the `HTML <nobr> tags:` 
+
+`Example:` 
+
+
+```html
+<!Doctype html><html> <head><title>HTML nobr Tag</title><isindex prompt="Search" /><style>h1 {color: green;}</style></head> <body><center><h1>GeeksforGeeks</h1><h4>HTML &lt;nobr&gt; tag</h4><nobr>Articles that need little modification/improvementfrom reviewers are published first. To quickly get yourarticles reviewed, please refer existing articles, theirformating style, coding style, and try to make your closeto them.</nobr></center></body> </html> |
+
+```
+`Output:`
+
+ ![](https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190827102845/Kazam_screencast_00000-1.gif) 
+
+`Supported Browsers:` The browsers supported by `HTML <nobr>` tag are listed below:
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Safari
+* Opera
+The `<nobr>` tag is used to create a single line text, that does not matter how long the statement is, this tag used with [`<wbr>`](https://www.geeksforgeeks.org/html-5-wbr-tag/) tag. The created single line statement bring the horizontal scroll to read the whole line. The `<nobr>` tag is exact opposite of [<br>](https://www.geeksforgeeks.org/html-br-tag/) tag. You can use the CSS white space property as a replacement of this tag. 
+
+`Note:` <nobr> tag is not supported in html5.
+
+`Syntax:`
+
+
+
+<nobr> Statement </nobr>
+
+`Attribute:` This tag doesn’t contain any attribute. Below example illustrates the `HTML <nobr> tags:` 
+
+`Example:` 
+
+
+```html
+<!Doctype html><html> <head><title>HTML nobr Tag</title><isindex prompt="Search" /><style>h1 {color: green;}</style></head> <body><center><h1>GeeksforGeeks</h1><h4>HTML &lt;nobr&gt; tag</h4><nobr>Articles that need little modification/improvementfrom reviewers are published first. To quickly get yourarticles reviewed, please refer existing articles, theirformating style, coding style, and try to make your closeto them.</nobr></center></body> </html> |
+
+```
+`Output:`
+
+ ![](https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190827102845/Kazam_screencast_00000-1.gif) 
+
+`Supported Browsers:` The browsers supported by `HTML <nobr>` tag are listed below:
+
+* Google Chrome
+* Internet Explorer
+* Firefox
+* Safari
+* Opera
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="nobr",
+        )
+
+        
+
