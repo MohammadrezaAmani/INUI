61c9bdd4f9bb6cfeaf563c9ba98c8aaeabcba51f
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 16:34:52 +0330

    add Code class

--- inui/elements.py
+++ inui/elements.py
@@ -34270,3 +34270,254 @@ ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksfo
 
         
 
+
+class Center(BaseElement):
+    """
+The `<center> tag` in HTML is used to set the alignment of text into the center. This tag is not supported in HTML5. CSS’s property is used to set the alignment of the element instead of the center tag in HTML5.
+
+`Syntax:`
+
+
+
+<center> Contents... </center>
+
+`Attributes:` This tag does not accept any attribute.
+
+`Example 1:` In this example, we simply used the Hi Geeks content in the HTML center tag.
+
+```html
+<!DOCTYPE html><html> <body><h2>Welcome to GeeksforGeeks</h2><!--center tag is used here--><center>Hi Geeks</center> </body> </html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210916173151/162.png)HTML <center> tag
+
+`Example 2:` This example illustrates the HTML center tag. 
+
+```html
+<!DOCTYPE html><html><body><!--center tag starts here --><center><h1>GeeksforGeeks</h1><h2>html center tag</h2> <p>GeeksforGeeks: A computer science portal for geeks</p> <!--center tag ends here --></center></body></html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20220914155852/htmlcentertag.png) 
+
+`Example 3:` Use CSS property in HTML5 to set the text alignment into the center. 
+
+```html
+<!DOCTYPE html><html><body><h1>GeeksforGeeks</h1><!--center using CSS style  --><h2 style="text-align: center">CSS center property</h2></body></html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210208122101/scrn.png)HTML <center> tag
+
+`Supported Browsers:`
+
+* Google Chrome
+* Microsoft Edge 12 and above
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+
+  
+The `<center> tag` in HTML is used to set the alignment of text into the center. This tag is not supported in HTML5. CSS’s property is used to set the alignment of the element instead of the center tag in HTML5.
+
+`Syntax:`
+
+
+
+<center> Contents... </center>
+
+`Attributes:` This tag does not accept any attribute.
+
+`Example 1:` In this example, we simply used the Hi Geeks content in the HTML center tag.
+
+```html
+<!DOCTYPE html><html> <body><h2>Welcome to GeeksforGeeks</h2><!--center tag is used here--><center>Hi Geeks</center> </body> </html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210916173151/162.png)HTML <center> tag
+
+`Example 2:` This example illustrates the HTML center tag. 
+
+```html
+<!DOCTYPE html><html><body><!--center tag starts here --><center><h1>GeeksforGeeks</h1><h2>html center tag</h2> <p>GeeksforGeeks: A computer science portal for geeks</p> <!--center tag ends here --></center></body></html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20220914155852/htmlcentertag.png) 
+
+`Example 3:` Use CSS property in HTML5 to set the text alignment into the center. 
+
+```html
+<!DOCTYPE html><html><body><h1>GeeksforGeeks</h1><!--center using CSS style  --><h2 style="text-align: center">CSS center property</h2></body></html> |
+
+```
+`Output:`
+
+![](https://media.geeksforgeeks.org/wp-content/uploads/20210208122101/scrn.png)HTML <center> tag
+
+`Supported Browsers:`
+
+* Google Chrome
+* Microsoft Edge 12 and above
+* Internet Explorer
+* Firefox
+* Opera
+* Safari
+
+  
+
+
+ref = [https://www.geeksforgeeks.org/html-tags-a-to-z-list/](https://www.geeksforgeeks.org/html-tags-a-to-z-list/)
+"""
+
+    def __init__(
+        self,
+        data=(),
+        attributes={},
+        classs=None,
+        id=None,
+        src=None,
+        name=None,
+        content=None,
+        charset=None,
+        style=None,
+        href=None,
+        autocapitalize=None,
+        accesskey=None,
+        accessskey=None,
+        autofocus=None,
+        contenteditable=None,
+        dir=None,
+        draggable=None,
+        enterkeyhint=None,
+        exportparts=None,
+        hidden=None,
+        inert=None,
+        inputmode=None,
+        iss=None,
+        itemid=None,
+        itemprop=None,
+        itemref=None,
+        itemscope=None,
+        itemtype=None,
+        lang=None,
+        nonce=None,
+        part=None,
+        popover=None,
+        slot=None,
+        spellcheck=None,
+        tabindex=None,
+        title=None,
+        translate=None,
+        virtualkeyboardpolicy=None,
+        accept=None,
+        autocomplete=None,
+        capture=None,
+        crossorigin=None,
+        selected=None,
+        dirname=None,
+        disabled=None,
+        elementtiming=None,
+        forr=None,
+        max=None,
+        maxlength=None,
+        min=None,
+        minlength=None,
+        multiple=None,
+        pattern=None,
+        readonly=None,
+        rel=None,
+        required=None,
+        size=None,
+        step=None,
+        typee=None,
+        placeholder=None,
+        text=None,
+        scope=None,
+        colspan=None,
+        aria_describedby=None,
+        aria_label=None,
+        alt=None,
+    ):
+        super().__init__(
+            data=data,
+            attributes=attributes,
+            classs=classs,
+            id=id,
+            src=src,
+            name=name,
+            content=content,
+            charset=charset,
+            style=style,
+            href=href,
+            autocapitalize=autocapitalize,
+            accesskey=accesskey,
+            accessskey=accessskey,
+            autofocus=autofocus,
+            contenteditable=contenteditable,
+            dir=dir,
+            draggable=draggable,
+            enterkeyhint=enterkeyhint,
+            exportparts=exportparts,
+            hidden=hidden,
+            inert=inert,
+            inputmode=inputmode,
+            iss=iss,
+            itemid=itemid,
+            itemprop=itemprop,
+            itemref=itemref,
+            itemscope=itemscope,
+            itemtype=itemtype,
+            lang=lang,
+            nonce=nonce,
+            part=part,
+            popover=popover,
+            slot=slot,
+            spellcheck=spellcheck,
+            tabindex=tabindex,
+            title=title,
+            translate=translate,
+            virtualkeyboardpolicy=virtualkeyboardpolicy,
+            accept=accept,
+            autocomplete=autocomplete,
+            capture=capture,
+            crossorigin=crossorigin,
+            selected=selected,
+            dirname=dirname,
+            disabled=disabled,
+            elementtiming=elementtiming,
+            forr=forr,
+            max=max,
+            maxlength=maxlength,
+            min=min,
+            minlength=minlength,
+            multiple=multiple,
+            pattern=pattern,
+            readonly=readonly,
+            rel=rel,
+            required=required,
+            size=size,
+            step=step,
+            typee=typee,
+            placeholder=placeholder,
+            text=text,
+            scope=scope,
+            colspan=colspan,
+            aria_describedby=aria_describedby,
+            aria_label=aria_label,
+            alt=alt,
+            startTagName=None,
+            endTagName=None,
+            tagName="center",
+        )
+
+        
+
