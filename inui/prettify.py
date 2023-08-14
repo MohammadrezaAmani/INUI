2c7cc9ce3c1ea07277ce3fe5114863441d07e107
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-14 15:22:23 +0330

    add Pretiify Support to project

---
+++ inui/prettify.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup as bs
+
+
+class Pretiffy():
+    def __init__(self, text='', filePath=''):
+        self.filePath = filePath
+        self.text = text
+        if self.filePath != '':
+            self.fromFile()
+
+    def save(self, out='out.html'):
+        with open(out, 'w', encoding='utf-8') as fi:
+            fi.write(self.pretiffy_string())
+
+    def fromFile(self):
+        if self.filePath != '':
+            with open(self.filePath, 'r', encoding='utf-8') as f:
+                self.text = f.read()
+
+    def pretiffy_string(self):
+        soup = bs(self.text, 'html.parser')
+        return soup.prettify()
+
+    def __str__(self) -> str:
+        return self.pretiffy_string()
+
+    def __repr__(self) -> str:
+        return self.pretiffy_string()
