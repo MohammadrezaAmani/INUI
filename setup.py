0691cead1cea83fb0f9aba848d6b9a7a89e3ccf8
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-15 04:20:31 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,18 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-# This gets deployed when a new release is made by github actions
+
 VERSION = '0.1.2'
 
-# CHANGEME VARS
 PACKAGE_NAME = "inui"
 DESCRIPTION = ''
 LONG_DESCRIPTION = 'Powerful and Highly Customizable Python Library for UI'
 AUTHOR_NAME = "Mohammadreza Amani"
 AUTHOR_EMAIL = "more.amani@yahoo.com"
 PROJECT_URL = "https://github.com/MohammadrezaAmani/INUI/"
-REQUIRED_PACKAGES = ['beautifulsoup4'] # required 3rd party tools used by your package
+REQUIRED_PACKAGES = ['beautifulsoup4']
 PROJECT_KEYWORDS = ['ui', 'python', 'html', 'framework','frontend','bootstrap']
-# Read more about classifiers at
-# https://pypi.org/classifiers/
+
 CLASSIFIERS = [
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
