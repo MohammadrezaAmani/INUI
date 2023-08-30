7e2fd6a8f4b1c1c6c6d8bd1b704a637b5e3df328
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:13:32 +0330

    adding template: app-dashboard-layout.zip

---
+++ inui/foundation/blocks/app-dashboard-layout/app-dashboard-layout.js
@@ -0,0 +1,4 @@
+$('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
+  e.preventDefault();
+  $(this).parents('.app-dashboard').toggleClass('shrink-medium').toggleClass('shrink-large');
+});
