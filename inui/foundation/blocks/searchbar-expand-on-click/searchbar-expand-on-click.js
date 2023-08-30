2a2d78b9c98012a8de36b10ea30a8db3ec539eb3
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:08:21 +0330

    adding template: searchbar-expand-on-click.zip

---
+++ inui/foundation/blocks/searchbar-expand-on-click/searchbar-expand-on-click.js
@@ -0,0 +1,13 @@
+$(document).foundation();
+
+$(function() {
+  $('.search')
+    .bind('click', function(event) {
+      $(".search-field").toggleClass("expand-search");
+
+      // if the search field is expanded, focus on it
+      if ($(".search-field").hasClass("expand-search")) {
+        $(".search-field").focus();
+      }
+    })
+});
