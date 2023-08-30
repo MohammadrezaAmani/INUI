0a5de1e377680ea205196c1648f258c5edb05a70
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:13 +0330

    updating template: portfolio_hero_header

---
+++ inui/foundation/blocks/portfolio_hero_header/portfolio_hero_header.py
@@ -0,0 +1,69 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Header(
+                    classs='''portfolio-hero-header''',
+                    data = (
+                        Img(
+                            classs='''portfolio-hero-header-logo''',
+                            src='''https://placehold.it/200''',
+                        ), 
+                        H1(
+                            classs='''portfolio-hero-header-h1''',
+                            data = ('''My Portfolio''',)
+                        ), 
+                        Ul(
+                            classs='''portfolio-hero-header-description''',
+                            data = (
+                                Li(
+
+                                    data = ('''Portfolio Example''',)
+                                ), 
+                                Li(
+
+                                    data = ('''Super Clean''',)
+                                ), 
+                                Li(
+
+                                    data = ('''Super Simple''',)
+                                ), )
+                        ), 
+                        Ul(
+                            classs='''portfolio-hero-header-menu''',
+                            data = (
+                                Li(
+
+                                    data = (
+                                        A(
+                                            classs='''button primary''',
+                                            href='''#''',
+                                            data = ('''Link 1''',)
+                                        ), )
+                                ), 
+                                Li(
+
+                                    data = (
+                                        A(
+                                            classs='''button primary''',
+                                            href='''#''',
+                                            data = ('''Link 2''',)
+                                        ), )
+                                ), 
+                                Li(
+
+                                    data = (
+                                        A(
+                                            classs='''button primary''',
+                                            href='''#''',
+                                            data = ('''Link 3''',)
+                                        ), )
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
