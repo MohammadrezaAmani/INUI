aa312aa550ef7b44abc5b8e3dffb89eb2190a52b
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:08:08 +0330

    adding template: star-rating.zip

---
+++ inui/foundation/blocks/star-rating/star-rating.js
@@ -0,0 +1,8 @@
+$('[data-rating] .star').on('click', function() {
+  var selectedCssClass = 'selected';
+  var $this = $(this);
+  $this.siblings('.' + selectedCssClass).removeClass(selectedCssClass);
+  $this
+    .addClass(selectedCssClass)
+    .parent().addClass('is-voted');
+});
