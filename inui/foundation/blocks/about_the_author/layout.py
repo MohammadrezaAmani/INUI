7e33dafa51ae600309a88030f19caa2c7f5d04cf
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:16 +0330

    updating template: about_the_author

---
+++ inui/foundation/blocks/about_the_author/layout.py
@@ -0,0 +1,16 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row medium-12 large-8 column align-center''',
+                    data = ('''
+  {{> about-the-author}}
+''',)
+                ), )
+        ), )
+)
