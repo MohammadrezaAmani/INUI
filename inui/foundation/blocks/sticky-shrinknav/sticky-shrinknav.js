cebc2b3432411c238b360ef256c545794322b356
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:08:02 +0330

    adding template: sticky-shrinknav.zip

---
+++ inui/foundation/blocks/sticky-shrinknav/sticky-shrinknav.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  });
+});
