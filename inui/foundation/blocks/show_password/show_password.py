733e156157b01ba58e2f6974a93703aed4b5d67d
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:12 +0330

    updating template: show_password

---
+++ inui/foundation/blocks/show_password/show_password.py
@@ -0,0 +1,45 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Form(
+                    classs='''show-password''',
+                    data = (
+                        Label(
+                            forr='''username''',
+                            data = ('''Your login''',)
+                        ), 
+                        Input(
+                            typee='''text''',
+                            value='',
+                            placeholder='''Enter Username''',
+                            id='''username''',
+                        ), 
+                        Div(
+                            classs='''password-wrapper''',
+                            data = (
+                                Label(
+                                    forr='''password''',
+                                    data = ('''Your password''',)
+                                ), 
+                                Input(
+                                    typee='''password''',
+                                    value='',
+                                    placeholder='''Enter Password''',
+                                    id='''password''',
+                                    classs='''password''',
+                                ), 
+                                Button(
+                                    classs='''unmask''',
+                                    typee='''button''',
+                                    title='''Mask/Unmask password to check content''',
+                                    data = ('''Unmask''',)
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
