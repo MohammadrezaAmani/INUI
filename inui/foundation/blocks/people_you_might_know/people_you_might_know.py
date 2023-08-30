f14cca624d0656c050e53f04b25143e624188394
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:13 +0330

    updating template: people_you_might_know

---
+++ inui/foundation/blocks/people_you_might_know/people_you_might_know.py
@@ -0,0 +1,238 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''people-you-might-know''',
+                    data = (
+                        Div(
+                            classs='''add-people-header''',
+                            data = (
+                                H6(
+                                    classs='''header-title''',
+                                    data = ('''
+      People You Might Know
+    ''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''row add-people-section''',
+                            data = (
+                                Div(
+                                    classs='''small-12 medium-6 columns about-people''',
+                                    data = (
+                                        Div(
+                                            classs='''about-people-avatar''',
+                                            data = (
+                                                Img(
+                                                    classs='''avatar-image''',
+                                                    src='''https://i.imgur.com/UPVxPjb.jpg''',
+                                                    alt='''Kishore Kumar''',
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''about-people-author''',
+                                            data = (
+                                                P(
+                                                    classs='''author-name''',
+                                                    data = ('''
+          Kishore Kumar
+        ''',)
+                                                ), 
+                                                P(
+                                                    classs='''author-location''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-map-marker''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), 
+                                                P(
+                                                    classs='''author-mutual''',
+                                                    data = (
+                                                        Strong(
+
+                                                            data = ('''Shahrukh Khan''',)
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), 
+                                Div(
+                                    classs='''small-12 medium-6 columns add-friend''',
+                                    data = (
+                                        Div(
+                                            classs='''add-friend-action''',
+                                            data = (
+                                                Button(
+                                                    classs='''button primary small''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-user-plus''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), 
+                                                Button(
+                                                    classs='''button secondary small''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-user-times''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), )
+                        ), 
+                        Div(
+                            classs='''row add-people-section''',
+                            data = (
+                                Div(
+                                    classs='''small-12 medium-6 columns about-people''',
+                                    data = (
+                                        Div(
+                                            classs='''about-people-avatar''',
+                                            data = (
+                                                Img(
+                                                    classs='''avatar-image''',
+                                                    src='''https://i.imgur.com/GHeazQ2.jpg''',
+                                                    alt='''Kishore Kumar''',
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''about-people-author''',
+                                            data = (
+                                                P(
+                                                    classs='''author-name''',
+                                                    data = ('''
+          Barack Obama
+        ''',)
+                                                ), 
+                                                P(
+                                                    classs='''author-location''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-map-marker''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), 
+                                                P(
+                                                    classs='''author-mutual''',
+                                                    data = (
+                                                        Strong(
+
+                                                            data = ('''Hilary Clinton''',)
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), 
+                                Div(
+                                    classs='''small-12 medium-6 columns add-friend''',
+                                    data = (
+                                        Div(
+                                            classs='''add-friend-action''',
+                                            data = (
+                                                Button(
+                                                    classs='''button primary small''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-user-plus''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), 
+                                                Button(
+                                                    classs='''button secondary small''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-user-times''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), )
+                        ), 
+                        Div(
+                            classs='''row add-people-section''',
+                            data = (
+                                Div(
+                                    classs='''small-12 medium-6 columns about-people''',
+                                    data = (
+                                        Div(
+                                            classs='''about-people-avatar''',
+                                            data = (
+                                                Img(
+                                                    classs='''avatar-image''',
+                                                    src='''https://i.imgur.com/SytPzuC.jpg''',
+                                                    alt='''Kishore Kumar''',
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''about-people-author''',
+                                            data = (
+                                                P(
+                                                    classs='''author-name''',
+                                                    data = ('''
+          Harry Manchanda
+        ''',)
+                                                ), 
+                                                P(
+                                                    classs='''author-location''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-map-marker''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), 
+                                                P(
+                                                    classs='''author-mutual''',
+                                                    data = (
+                                                        Strong(
+
+                                                            data = ('''Rafi Benkual''',)
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), 
+                                Div(
+                                    classs='''small-12 medium-6 columns add-friend''',
+                                    data = (
+                                        Div(
+                                            classs='''add-friend-action''',
+                                            data = (
+                                                Button(
+                                                    classs='''button primary small''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-user-plus''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), 
+                                                Button(
+                                                    classs='''button secondary small''',
+                                                    data = (
+                                                        I(
+                                                            classs='''fa fa-user-times''',
+                                                            aria_hidden='''true''',
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), )
+                        ), 
+                        Div(
+                            classs='''view-more-people''',
+                            data = (
+                                P(
+                                    classs='''view-more-text''',
+                                    data = (
+                                        A(
+                                            href='''#''',
+                                            classs='''view-more-link''',
+                                            data = ('''View More..''',)
+                                        ), )
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
