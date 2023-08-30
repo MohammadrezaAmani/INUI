80027ce1ee0bcd4df38944c74d3f78048b21833d
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:13 +0330

    updating template: plus_minus_input

---
+++ inui/foundation/blocks/plus_minus_input/layout.py
@@ -0,0 +1,24 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row''',
+                    data = (
+                        Div(
+                            classs='''columns''',
+                            data = (
+                                Div(
+                                    classs='''demo-center''',
+                                    data = ('''
+      {{> plus-minus-input}}
+    ''',)
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
