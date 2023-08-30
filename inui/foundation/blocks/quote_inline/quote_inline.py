47818af1a305b2409b148500effd9a148b58f5a5
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:12 +0330

    updating template: quote_inline

---
+++ inui/foundation/blocks/quote_inline/quote_inline.py
@@ -0,0 +1,77 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''quote-inline-wrap-article-content''',
+                    data = (
+                        Div(
+                            classs='''quote-inline-wrap''',
+                            data = (
+                                Div(
+                                    classs='''quote-inline-marks''',
+                                    data = ('''“''',)
+                                ), 
+                                Div(
+                                    classs='''quote-inline-testimonial''',
+                                    data = (
+                                        P(
+
+                                            data = ('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices, elit sed faucibus pharetra, diam mauris bibendum orci, sit amet ullamcorper purus dui sit amet augue. Donec aliquet diam ut neque mattis, eu tristique sem rutrum.''',)
+                                        ), )
+                                ), 
+                                Div(
+                                    classs='''quote-inline-person''',
+                                    data = (
+                                        Div(
+                                            classs='''quote-inline-photo''',
+                                            data = (
+                                                Img(
+                                                    src='''https://placehold.it/70''',
+                                                    classs='',
+                                                    height='',
+                                                    width='',
+                                                    alt='',
+                                                ), )
+                                        ), 
+                                        P(
+
+                                            data = ('''John Doe''',)
+                                        ), 
+                                        P(
+
+                                            data = ('''Important person, some Company''',)
+                                        ), )
+                                ), )
+                        ), 
+                        P(
+
+                            data = ('''Flank spare ribs capicola, strip steak biltong pancetta bresaola tri-tip cow landjaeger. Short ribs sirloin beef ribs, flank capicola ribeye turducken. Sirloin boudin andouille tail. Ham flank tail sausage t-bone, jerky landjaeger kevin porchetta ground round pork belly.''',)
+                        ), 
+                        P(
+
+                            data = ('''Pork loin doner biltong shoulder meatball flank. Sirloin shankle ground round tail, short loin prosciutto beef ribs salami pork pancetta kielbasa. Brisket spare ribs jerky filet mignon flank. Turducken ham sausage pork loin salami. Ball tip meatloaf shank pancetta hamburger meatball. Jowl beef pork loin tenderloin turducken, pastrami capicola filet mignon ham hock pork belly t-bone jerky ball tip kevin.''',)
+                        ), 
+                        P(
+
+                            data = ('''Filet mignon shank pastrami pork loin ham jerky corned beef chuck drumstick ribeye prosciutto pork belly tail. Beef leberkas brisket ham hock tenderloin tail tri-tip pork chop. Corned beef boudin andouille tail fatback chicken strip steak biltong pancetta pork loin tri-tip landjaeger pastrami kevin. Tongue spare ribs kevin ribeye shoulder swine.''',)
+                        ), 
+                        P(
+
+                            data = ('''Chicken kielbasa swine ham hock filet mignon strip steak bacon fatback pork t-bone. Ground round shankle tongue, jowl flank spare ribs beef leberkas ham boudin shank capicola short ribs. Drumstick pork loin turkey pork chop salami andouille pork belly. Short loin tongue tri-tip, hamburger filet mignon pastrami doner shoulder turkey ball tip t-bone jerky ham chicken.''',)
+                        ), 
+                        P(
+
+                            data = ('''Ground round flank pork loin salami, cow biltong hamburger swine brisket tongue tenderloin drumstick meatball. Biltong tongue salami ham shankle, leberkas meatloaf sirloin ball tip. Pork loin pastrami spare ribs beef capicola sausage kevin filet mignon pork chop. Bacon jerky turkey shoulder cow capicola ball tip shank. Ham tongue tail tri-tip kielbasa ham hock frankfurter meatball cow brisket. Shankle sausage filet mignon biltong pork loin. Short ribs spare ribs chuck pig meatloaf shoulder cow tail brisket biltong ball tip ham hock.''',)
+                        ), 
+                        P(
+
+                            data = ('''Biltong salami rump drumstick pork belly bacon frankfurter pork chop pork meatloaf cow. Shankle chuck venison landjaeger jerky sirloin. Chicken kevin strip steak, turkey sausage brisket meatloaf prosciutto frankfurter fatback pancetta pork boudin. Landjaeger pastrami biltong short loin tongue filet mignon leberkas pig. Biltong tenderloin ground round, short ribs hamburger ribeye strip steak ham corned beef porchetta.''',)
+                        ), )
+                ), )
+        ), )
+)
