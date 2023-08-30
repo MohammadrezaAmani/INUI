a11d032fbc02ed4fccc8ea3cc416065dd176cea3
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:14 +0330

    updating template: marketing_site_hero

---
+++ inui/foundation/blocks/marketing_site_hero/marketing_site_hero.py
@@ -0,0 +1,31 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''marketing-site-hero''',
+                    data = (
+                        Div(
+                            classs='''marketing-site-hero-content''',
+                            data = (
+                                H1(
+
+                                    data = ('''Yeti Snowcone Agency''',)
+                                ), 
+                                P(
+                                    classs='''subheader''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam omnis, maxime libero natus qui minus!''',)
+                                ), 
+                                A(
+                                    href='''#''',
+                                    classs='''round button''',
+                                    data = ('''learn more''',)
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
