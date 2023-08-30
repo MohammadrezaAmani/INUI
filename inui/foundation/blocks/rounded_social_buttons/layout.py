6da4e89b95d75a8ad661ea23455966954a89c5ca
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:12 +0330

    updating template: rounded_social_buttons

---
+++ inui/foundation/blocks/rounded_social_buttons/layout.py
@@ -0,0 +1,16 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row large-8 columns align-center''',
+                    data = ('''
+  {{>rounded-social-buttons}}
+''',)
+                ), )
+        ), )
+)
