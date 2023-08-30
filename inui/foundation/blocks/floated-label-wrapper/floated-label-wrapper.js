7970b0cef77b5f0a5620c9bcfcaa305cf394f5c4
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:11:07 +0330

    adding template: floated-label-wrapper.zip

---
+++ inui/foundation/blocks/floated-label-wrapper/floated-label-wrapper.js
@@ -0,0 +1,14 @@
+$(function () {
+  var showClass = 'show';
+
+  $('input').on('checkval', function () {
+    var label = $(this).prev('label');
+    if(this.value !== '') {
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  }).on('keyup', function () {
+    $(this).trigger('checkval');
+  });
+});
