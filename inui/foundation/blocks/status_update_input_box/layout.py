923e4226d305a3684dab39e237f0060faeea1dbd
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:12 +0330

    updating template: status_update_input_box

---
+++ inui/foundation/blocks/status_update_input_box/layout.py
@@ -0,0 +1,16 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row medium-12 large-6 column align-center''',
+                    data = ('''
+  {{> status-update-input-box}}
+''',)
+                ), )
+        ), )
+)
