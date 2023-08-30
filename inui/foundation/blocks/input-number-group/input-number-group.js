637e46834b57d22d53d952c14bd7223102acfc77
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:10:22 +0330

    adding template: input-number-group.zip

---
+++ inui/foundation/blocks/input-number-group/input-number-group.js
@@ -0,0 +1,11 @@
+$('.input-number-increment').click(function() {
+  var $input = $(this).parents('.input-number-group').find('.input-number');
+  var val = parseInt($input.val(), 10);
+  $input.val(val + 1);
+});
+
+$('.input-number-decrement').click(function() {
+  var $input = $(this).parents('.input-number-group').find('.input-number');
+  var val = parseInt($input.val(), 10);
+  $input.val(val - 1);
+})
