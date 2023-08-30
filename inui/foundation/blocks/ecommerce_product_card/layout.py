0c97da5926c0234da76751bb49e0c0f365b18a56
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:15 +0330

    updating template: ecommerce_product_card

---
+++ inui/foundation/blocks/ecommerce_product_card/layout.py
@@ -0,0 +1,44 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row''',
+                    data = (
+                        Div(
+                            classs='''column''',
+                            data = ('''
+    {{> ecommerce-product-card}}
+  ''',)
+                        ), 
+                        Div(
+                            classs='''column''',
+                            data = ('''
+    {{> ecommerce-product-card}}
+  ''',)
+                        ), 
+                        Div(
+                            classs='''column''',
+                            data = ('''
+    {{> ecommerce-product-card}}
+  ''',)
+                        ), 
+                        Div(
+                            classs='''column''',
+                            data = ('''
+    {{> ecommerce-product-card}}
+  ''',)
+                        ), 
+                        Div(
+                            classs='''column''',
+                            data = ('''
+    {{> ecommerce-product-card}}
+  ''',)
+                        ), )
+                ), )
+        ), )
+)
