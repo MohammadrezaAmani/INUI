fcab4e2d7725bad003c257e4254363c7341c9231
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:14 +0330

    updating template: input_group_rounded

---
+++ inui/foundation/blocks/input_group_rounded/input_group_rounded.py
@@ -0,0 +1,27 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''input-group input-group-rounded''',
+                    data = (
+                        Input(
+                            classs='''input-group-field''',
+                            typee='''search''',
+                        ), 
+                        Div(
+                            classs='''input-group-button''',
+                            data = (
+                                Input(
+                                    typee='''submit''',
+                                    classs='''button secondary''',
+                                    value='''Search''',
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
