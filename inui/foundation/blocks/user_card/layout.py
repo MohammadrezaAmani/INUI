458f7a49dbf441a85fddb796d21690c53d0ca8d8
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:11 +0330

    updating template: user_card

---
+++ inui/foundation/blocks/user_card/layout.py
@@ -0,0 +1,290 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row small-up-1 medium-up-2 large-up-4''',
+                    data = (
+                        Div(
+                            classs='''column''',
+                            data = (
+                                Div(
+                                    classs='''card-user-container''',
+                                    data = (
+                                        Div(
+                                            classs='''card-user-avatar''',
+                                            data = (
+                                                Img(
+                                                    src='''https://placehold.it/200x200''',
+                                                    alt='',
+                                                    classs='''user-image''',
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-bio''',
+                                            data = (
+                                                H4(
+
+                                                    data = ('''User Name''',)
+                                                ), 
+                                                P(
+
+                                                    data = ('''UX/UI ,Front-end developer, Foundation interested. ''',)
+                                                ), 
+                                                Span(
+                                                    classs='''location''',
+                                                    data = (
+                                                        Span(
+                                                            classs='''location-icon fa fa-map-marker''',
+                                                        ), 
+                                                        Span(
+                                                            classs='''location-text''',
+                                                            data = ('''Makkah Al-Mukaramah''',)
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-social''',
+                                            data = (
+                                                Ul(
+                                                    classs='''menu''',
+                                                    data = (
+                                                        Li(
+                                                            classs='''fa fa-twitter''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-dribbble''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-instagram''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-github''',
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-button''',
+                                            data = (
+                                                A(
+                                                    href='''#''',
+                                                    classs='''hollow button''',
+                                                    data = ('''FOLLOW''',)
+                                                ), )
+                                        ), )
+                                ), )
+                        ), 
+                        Div(
+                            classs='''column''',
+                            data = (
+                                Div(
+                                    classs='''card-user-container''',
+                                    data = (
+                                        Div(
+                                            classs='''card-user-avatar''',
+                                            data = (
+                                                Img(
+                                                    src='''https://placehold.it/350x350''',
+                                                    alt='',
+                                                    classs='''user-image''',
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-social''',
+                                            data = (
+                                                Ul(
+                                                    classs='''menu''',
+                                                    data = (
+                                                        Li(
+                                                            classs='''fa fa-twitter''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-dribbble''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-instagram''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-github''',
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-bio''',
+                                            data = (
+                                                H4(
+
+                                                    data = ('''User Name''',)
+                                                ), 
+                                                P(
+
+                                                    data = ('''UX/UI ,Front-end developer, Foundation interested. ''',)
+                                                ), 
+                                                Span(
+                                                    classs='''location''',
+                                                    data = (
+                                                        Span(
+                                                            classs='''location-icon fa fa-map-marker''',
+                                                        ), 
+                                                        Span(
+                                                            classs='''location-text''',
+                                                            data = ('''Makkah Al-Mukaramah''',)
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-button''',
+                                            data = (
+                                                A(
+                                                    href='''#''',
+                                                    classs='''button''',
+                                                    data = ('''FOLLOW''',)
+                                                ), )
+                                        ), )
+                                ), )
+                        ), 
+                        Div(
+                            classs='''column''',
+                            data = (
+                                Div(
+                                    classs='''card-user-container''',
+                                    data = (
+                                        Div(
+                                            classs='''card-user-avatar''',
+                                            data = (
+                                                Img(
+                                                    src='''https://placehold.it/200x200''',
+                                                    alt='',
+                                                    classs='''user-image''',
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-social''',
+                                            data = (
+                                                Ul(
+                                                    classs='''menu''',
+                                                    data = (
+                                                        Li(
+                                                            classs='''fa fa-twitter''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-dribbble''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-instagram''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-github''',
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-bio''',
+                                            data = (
+                                                H4(
+
+                                                    data = ('''User Name''',)
+                                                ), 
+                                                P(
+
+                                                    data = ('''UX/UI ,Front-end developer, Foundation interested. ''',)
+                                                ), 
+                                                Span(
+                                                    classs='''location''',
+                                                    data = (
+                                                        Span(
+                                                            classs='''location-icon fa fa-map-marker''',
+                                                        ), 
+                                                        Span(
+                                                            classs='''location-text''',
+                                                            data = ('''Makkah Al-Mukaramah''',)
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-button''',
+                                            data = (
+                                                A(
+                                                    href='''#''',
+                                                    classs='''hollow button''',
+                                                    data = ('''FOLLOW''',)
+                                                ), )
+                                        ), )
+                                ), )
+                        ), 
+                        Div(
+                            classs='''column''',
+                            data = (
+                                Div(
+                                    classs='''card-user-container''',
+                                    data = (
+                                        Div(
+                                            classs='''card-user-avatar''',
+                                            data = (
+                                                Img(
+                                                    src='''https://placehold.it/200x200''',
+                                                    alt='',
+                                                    classs='''user-image''',
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-bio''',
+                                            data = (
+                                                H4(
+
+                                                    data = ('''User Name''',)
+                                                ), 
+                                                P(
+
+                                                    data = ('''UX/UI ,Front-end developer, Foundation interested. ''',)
+                                                ), 
+                                                Span(
+                                                    classs='''location''',
+                                                    data = (
+                                                        Span(
+                                                            classs='''location-icon fa fa-map-marker''',
+                                                        ), 
+                                                        Span(
+                                                            classs='''location-text''',
+                                                            data = ('''Makkah Al-Mukaramah''',)
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-social''',
+                                            data = (
+                                                Ul(
+                                                    classs='''menu''',
+                                                    data = (
+                                                        Li(
+                                                            classs='''fa fa-twitter''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-dribbble''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-instagram''',
+                                                        ), 
+                                                        Li(
+                                                            classs='''fa fa-github''',
+                                                        ), )
+                                                ), )
+                                        ), 
+                                        Div(
+                                            classs='''card-user-button''',
+                                            data = (
+                                                A(
+                                                    href='''#''',
+                                                    classs='''button''',
+                                                    data = ('''FOLLOW''',)
+                                                ), )
+                                        ), )
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
