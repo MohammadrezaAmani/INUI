f7f82d620a334221271a266ee80918a5bb89af0e
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:11 +0330

    updating template: travel_feature_card

---
+++ inui/foundation/blocks/travel_feature_card/layout.py
@@ -0,0 +1,19 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row columns medium-10''',
+                    data = ('''
+  {{> travel-feature-card type="hotel" title="Hotel" date="Fri, May 13 - Mon, May 16"}}
+  ''',
+                        Br(
+
+                        ), )
+                ), )
+        ), )
+)
