2372275adcfebd974d54346bafc1a6ac820ec5b7
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:15 +0330

    updating template: comment_section

---
+++ inui/foundation/blocks/comment_section/layout.py
@@ -0,0 +1,20 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row align-center''',
+                    data = (
+                        Div(
+                            classs='''large-6 columns''',
+                            data = ('''
+    {{> comment-section}}
+  ''',)
+                        ), )
+                ), )
+        ), )
+)
