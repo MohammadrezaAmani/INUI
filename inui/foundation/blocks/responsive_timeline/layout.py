53c8f6697278ede23327db9ebc08a3278ba4d7b4
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:12 +0330

    updating template: responsive_timeline

---
+++ inui/foundation/blocks/responsive_timeline/layout.py
@@ -0,0 +1,22 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row align-center''',
+                    data = (
+                        Div(
+                            classs='''columns large-10''',
+                            data = (
+                                H2(
+                                    classs='''text-center''',
+                                    data = ('''Foundation's Beginnings''',)
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
