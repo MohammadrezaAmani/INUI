ee2fd45b1fef6843f5d248bac79da15186867598
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:14 +0330

    updating template: masonry_css

---
+++ inui/foundation/blocks/masonry_css/masonry_css.py
@@ -0,0 +1,193 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''masonry-css''',
+                    data = (
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/300x300''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labore, aspernatur nihil distinctio fugit sint facilis sunt eius fugiat iusto blanditiis tenetur alias ut.''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labore, a ut.''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Op.''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/300x600''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/300x200''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labore, aspernatur nihil distinctio fugit sint facilis sunt eius fugiat iusto blanditiis tenetur alias ut. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio beatae deserunt quisquam similique vitae hic eveniet, velit, natus unde tempora excepturi labore, veniam necessitatibus. Vitae, magnam sequi. Veritatis, cumque, iste?''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/700x200''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/350x400''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labo?''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, la, consectetur adipisicing elit. Odio beatae deserunt quisquam similique vitae hic eveniet, velit, natus unde tempora excepturi labore, veniam necessitatibus. Vitae, magnam sequi. Veritatis, cumque, iste?''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/450x400''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labore, aspernatur nihil distinctio fugit sint facilis sunt eius fam necessitatibus. Vitae, magnam sequi. Veritatis, cumque, iste?''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/350x80''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/400x400''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Img(
+                                    src='''https://placehold.it/440x400''',
+                                    classs='',
+                                    height='',
+                                    width='',
+                                    alt='',
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labore, aspernatur nihil distinctio fugit sint facilis sunt eius fam necessitatibus. Vitae, magnam sequi. Veritatis, cumque, iste?''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labore, aspernatur nihil distinctio fugit sint facilis sunt eius fam necessitatibus. Vitae, magnam sequi. Veritatis, cumque, iste?''',)
+                                ), )
+                        ), 
+                        Div(
+                            classs='''masonry-css-item''',
+                            data = (
+                                Div(
+                                    classs='''callout''',
+                                    data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio aliquam, corrupti, quis rem beatae quidem, labore, aspernatur nihil distinctio fugit sint facilis sunt eius fam necessitatibus. Vitae, magnam sequi. Veritatis, cumque, iste?''',)
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
