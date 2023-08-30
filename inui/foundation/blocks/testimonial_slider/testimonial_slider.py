f1b2aa86529bfcc022cada075fa99d40e60b8dfe
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:11 +0330

    updating template: testimonial_slider

---
+++ inui/foundation/blocks/testimonial_slider/testimonial_slider.py
@@ -0,0 +1,249 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''orbit testimonial-slider-container''',
+                    role='''region''',
+                    aria_lable='''testimonial-slider''',
+                    data_orbit='',
+                    data = (
+                        Ul(
+                            classs='''orbit-container''',
+                            data = (
+                                Button(
+                                    classs='''orbit-previous''',
+                                    data = (
+                                        Span(
+                                            classs='''show-for-sr''',
+                                            data = ('''Previous Slide''',)
+                                        ), )
+                                ), 
+                                Button(
+                                    classs='''orbit-next''',
+                                    data = (
+                                        Span(
+                                            classs='''show-for-sr''',
+                                            data = ('''Next Slide''',)
+                                        ), )
+                                ), 
+                                Li(
+                                    classs='''is-active orbit-slide''',
+                                    data = (
+                                        Div(
+                                            classs='''testimonial-slide row''',
+                                            data = (
+                                                Div(
+                                                    classs='''small-12 large-9 column''',
+                                                    data = (
+                                                        Div(
+                                                            classs='''row align-middle testimonial-slide-content''',
+                                                            data = (
+                                                                Div(
+                                                                    classs='''small-12 medium-4 column hide-for-small-only profile-pic''',
+                                                                    data = (
+                                                                        Img(
+                                                                            src='''https://placeimg.com/300/300/nature''',
+                                                                        ), )
+                                                                ), 
+                                                                Div(
+                                                                    classs='''small-12 medium-8 column testimonial-slide-text''',
+                                                                    data = (
+                                                                        P(
+                                                                            classs='''testimonial-slide-quotation''',
+                                                                            data = ('''Hide when guests come over instantly break out into full speed gallop across the house for no reason mrow touch water with paw then recoil in horror.''',)
+                                                                        ), 
+                                                                        Div(
+                                                                            classs='''testimonial-slide-author-container''',
+                                                                            data = (
+                                                                                Div(
+                                                                                    classs='''small-profile-pic show-for-small-only''',
+                                                                                    data = (
+                                                                                        Img(
+                                                                                            src='''https://placeimg.com/50/50/nature''',
+                                                                                        ), )
+                                                                                ), 
+                                                                                P(
+                                                                                    classs='''testimonial-slide-author-info''',
+                                                                                    data = ('''Fleas Witherspoon''',
+                                                                                        Br(
+
+                                                                                        ), 
+                                                                                        I(
+                                                                                            classs='''subheader''',
+                                                                                            data = ('''Cat World Inc.''',)
+                                                                                        ), )
+                                                                                ), )
+                                                                        ), )
+                                                                ), )
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), 
+                                Li(
+                                    classs='''orbit-slide''',
+                                    data = (
+                                        Div(
+                                            classs='''testimonial-slide row''',
+                                            data = (
+                                                Div(
+                                                    classs='''small-12 large-9 column''',
+                                                    data = (
+                                                        Div(
+                                                            classs='''row align-middle testimonial-slide-content''',
+                                                            data = (
+                                                                Div(
+                                                                    classs='''small-12 medium-4 column hide-for-small-only profile-pic''',
+                                                                    data = (
+                                                                        Img(
+                                                                            src='''https://placeimg.com/300/300/architecture''',
+                                                                        ), )
+                                                                ), 
+                                                                Div(
+                                                                    classs='''small-12 medium-8 column testimonial-slide-text''',
+                                                                    data = (
+                                                                        P(
+                                                                            classs='''testimonial-slide-quotation''',
+                                                                            data = ('''Hide when guests come over instantly break out into full speed gallop across the house for no reason mrow touch water with paw then recoil in horror.''',)
+                                                                        ), 
+                                                                        Div(
+                                                                            classs='''testimonial-slide-author-container''',
+                                                                            data = (
+                                                                                Div(
+                                                                                    classs='''small-profile-pic show-for-small-only''',
+                                                                                    data = (
+                                                                                        Img(
+                                                                                            src='''https://placeimg.com/50/50/architecture''',
+                                                                                        ), )
+                                                                                ), 
+                                                                                P(
+                                                                                    classs='''testimonial-slide-author-info''',
+                                                                                    data = ('''Fleas Witherspoon''',
+                                                                                        Br(
+
+                                                                                        ), 
+                                                                                        I(
+                                                                                            classs='''subheader''',
+                                                                                            data = ('''Cat World Inc.''',)
+                                                                                        ), )
+                                                                                ), )
+                                                                        ), )
+                                                                ), )
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), 
+                                Li(
+                                    classs='''orbit-slide''',
+                                    data = (
+                                        Div(
+                                            classs='''testimonial-slide row''',
+                                            data = (
+                                                Div(
+                                                    classs='''small-12 large-9 column''',
+                                                    data = (
+                                                        Div(
+                                                            classs='''row align-middle testimonial-slide-content''',
+                                                            data = (
+                                                                Div(
+                                                                    classs='''small-12 medium-4 column hide-for-small-only profile-pic''',
+                                                                    data = (
+                                                                        Img(
+                                                                            src='''https://placeimg.com/300/300/animals''',
+                                                                        ), )
+                                                                ), 
+                                                                Div(
+                                                                    classs='''small-12 medium-8 column testimonial-slide-text''',
+                                                                    data = (
+                                                                        P(
+                                                                            classs='''testimonial-slide-quotation''',
+                                                                            data = ('''Hide when guests come over instantly break out into full speed gallop across the house for no reason mrow touch water with paw then recoil in horror.''',)
+                                                                        ), 
+                                                                        Div(
+                                                                            classs='''testimonial-slide-author-container''',
+                                                                            data = (
+                                                                                Div(
+                                                                                    classs='''small-profile-pic show-for-small-only''',
+                                                                                    data = (
+                                                                                        Img(
+                                                                                            src='''https://placeimg.com/50/50/animals''',
+                                                                                        ), )
+                                                                                ), 
+                                                                                P(
+                                                                                    classs='''testimonial-slide-author-info''',
+                                                                                    data = ('''Fleas Witherspoon''',
+                                                                                        Br(
+
+                                                                                        ), 
+                                                                                        I(
+                                                                                            classs='''subheader''',
+                                                                                            data = ('''Cat World Inc.''',)
+                                                                                        ), )
+                                                                                ), )
+                                                                        ), )
+                                                                ), )
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), 
+                                Li(
+                                    classs='''orbit-slide''',
+                                    data = (
+                                        Div(
+                                            classs='''testimonial-slide row''',
+                                            data = (
+                                                Div(
+                                                    classs='''small-12 large-9 column''',
+                                                    data = (
+                                                        Div(
+                                                            classs='''row align-middle testimonial-slide-content''',
+                                                            data = (
+                                                                Div(
+                                                                    classs='''small-12 medium-4 column hide-for-small-only profile-pic''',
+                                                                    data = (
+                                                                        Img(
+                                                                            src='''https://placeimg.com/300/300/any''',
+                                                                        ), )
+                                                                ), 
+                                                                Div(
+                                                                    classs='''small-12 medium-8 column testimonial-slide-text''',
+                                                                    data = (
+                                                                        P(
+                                                                            classs='''testimonial-slide-quotation''',
+                                                                            data = ('''Hide when guests come over instantly break out into full speed gallop across the house for no reason mrow touch water with paw then recoil in horror.''',)
+                                                                        ), 
+                                                                        Div(
+                                                                            classs='''testimonial-slide-author-container''',
+                                                                            data = (
+                                                                                Div(
+                                                                                    classs='''small-profile-pic show-for-small-only''',
+                                                                                    data = (
+                                                                                        Img(
+                                                                                            src='''https://placeimg.com/50/50/any''',
+                                                                                        ), )
+                                                                                ), 
+                                                                                P(
+                                                                                    classs='''testimonial-slide-author-info''',
+                                                                                    data = ('''Fleas Witherspoon''',
+                                                                                        Br(
+
+                                                                                        ), 
+                                                                                        I(
+                                                                                            classs='''subheader''',
+                                                                                            data = ('''Cat World Inc.''',)
+                                                                                        ), )
+                                                                                ), )
+                                                                        ), )
+                                                                ), )
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
