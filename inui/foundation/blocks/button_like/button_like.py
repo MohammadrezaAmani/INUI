075940e8d70fd82a3129f6ffc4188274169198fc
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:16 +0330

    updating template: button_like

---
+++ inui/foundation/blocks/button_like/button_like.py
@@ -0,0 +1,21 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Button(
+                    classs='''button button-like''',
+                    data = (
+                        I(
+                            classs='''fa fa-heart''',
+                        ), 
+                        Span(
+
+                            data = ('''Like''',)
+                        ), )
+                ), )
+        ), )
+)
