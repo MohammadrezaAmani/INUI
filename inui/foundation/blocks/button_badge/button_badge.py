5db547c2017d6360781923608979c293ee4f480c
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:16 +0330

    updating template: button_badge

---
+++ inui/foundation/blocks/button_badge/button_badge.py
@@ -0,0 +1,30 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Head(
+
+            data = (
+                Link(
+                    href='''https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css''',
+                    rel='''stylesheet''',
+                ), )
+        ), 
+        Body(
+
+            data = (
+                A(
+                    href='''#''',
+                    classs='''button-badge''',
+                    data = (
+                        I(
+                            classs='''fa fa-envelope''',
+                        ), 
+                        Span(
+                            classs='''badge alert''',
+                            data = ('''1''',)
+                        ), )
+                ), )
+        ), )
+)
