ba9862293678115e2d8717c07ff89ef20be98122
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:14 +0330

    updating template: full_width_testimonial

---
+++ inui/foundation/blocks/full_width_testimonial/full_width_testimonial.py
@@ -0,0 +1,63 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''full-width-testimonial''',
+                    data = (
+                        Div(
+                            classs='''full-width-testimonial-section''',
+                            data = (
+                                Div(
+                                    classs='''full-width-testimonial-icon text-center''',
+                                    data = (
+                                        Svg(
+                                            version='''1.1''',
+                                            id='''Layer_1''',
+                                            xmlns='''http://www.w3.org/2000/svg''',
+                                            xmlns__xlink='''http://www.w3.org/1999/xlink''',
+                                            x='''0px''',
+                                            y='''0px''',
+                                            width='''41px''',
+                                            height='''34px''',
+                                            viewbox='''-235 240 41 34''',
+                                            style='''enable-background:new -235 240 41 34;''',
+                                            xml__space='''preserve''',
+                                            data = (
+                                                Path(
+                                                    classs='''quote-path''',
+                                                    d='''M-231.3,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4
+            h8.1v12.8h-14.4V260.4z''',
+                                                ), 
+                                                Path(
+                                                    classs='''quote-path''',
+                                                    d='''M-212,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4h8.1
+            v12.8H-212V260.4z''',
+                                                ), )
+                                        ), )
+                                ), 
+                                Div(
+                                    classs='''full-width-testimonial-content''',
+                                    data = (
+                                        H5(
+                                            classs='''full-width-testimonial-text''',
+                                            data = ('''Lake Tahoe is the most beautiful place I have ever been to. I have traveled several places in the world, and there might be more beautiful places out there, but none that I have seen. I loved it!
+      ''',)
+                                        ), 
+                                        P(
+                                            classs='''full-width-testimonial-source''',
+                                            data = ('''Gavin Newman''',)
+                                        ), 
+                                        Span(
+                                            classs='''full-width-testimonial-source-context''',
+                                            data = ('''Traveled from San Francisco to Lake Tahoe''',)
+                                        ), )
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
