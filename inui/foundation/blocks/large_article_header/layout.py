2b3d2c2073c4c4a0f071760d10de4d5b0319b257
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:14 +0330

    updating template: large_article_header

---
+++ inui/foundation/blocks/large_article_header/layout.py
@@ -0,0 +1,20 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row expanded collapse''',
+                    data = (
+                        Div(
+                            classs='''column''',
+                            data = ('''
+    {{> large-article-header}}
+  ''',)
+                        ), )
+                ), )
+        ), )
+)
