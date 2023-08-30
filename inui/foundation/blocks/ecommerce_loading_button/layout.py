b04df4d32ecf49b85372685bcdf183f82a2e99c4
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:15 +0330

    updating template: ecommerce_loading_button

---
+++ inui/foundation/blocks/ecommerce_loading_button/layout.py
@@ -0,0 +1,16 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''row small-12 medium-8 large-6 columns align-center''',
+                    data = ('''
+  {{>ecommerce-loading-button}}
+''',)
+                ), )
+        ), )
+)
