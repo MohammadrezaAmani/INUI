a7b95ff252f206686f6aeddeed781a2bcb3c2ece
MohammadrezaAmani <more.amani@yahoo.com>
2023-08-30 15:34:12 +0330

    updating template: sticky_footer_css

---
+++ inui/foundation/blocks/sticky_footer_css/sticky_footer_css.py
@@ -0,0 +1,122 @@
+from inui.elements import *
+from inui.svg import *
+Html(
+
+    data = (
+        Body(
+
+            data = (
+                Div(
+                    classs='''sticky-footer-css''',
+                    data = (
+                        Div(
+                            classs='''row expanded flex-dir-column''',
+                            data = (
+                                Div(
+                                    classs='''columns shrink header text-center''',
+                                    data = (
+                                        P(
+
+                                            data = ('''Header''',)
+                                        ), 
+                                        Ul(
+                                            classs='''menu align-center''',
+                                            data = (
+                                                Li(
+
+                                                    data = (
+                                                        A(
+                                                            href='',
+                                                            data = ('''Link''',)
+                                                        ), )
+                                                ), 
+                                                Li(
+
+                                                    data = (
+                                                        A(
+                                                            href='',
+                                                            data = ('''Link''',)
+                                                        ), )
+                                                ), 
+                                                Li(
+
+                                                    data = (
+                                                        A(
+                                                            href='',
+                                                            data = ('''Link''',)
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), 
+                                Div(
+                                    classs='''columns sticky-footer-css-body text-center''',
+                                    data = (
+                                        P(
+
+                                            data = ('''Body Content''',)
+                                        ), 
+                                        P(
+
+                                            data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse beatae aspernatur tempore similique enim, doloribus et ullam quam itaque sit! Molestias, perspiciatis omnis illo aliquam dignissimos laborum iste? Autem, esse?''',)
+                                        ), 
+                                        P(
+
+                                            data = (
+                                                Img(
+                                                    src='''https://placeholder.pics/svg/300x300/1797FF-773BFF''',
+                                                    alt='',
+                                                ), 
+                                                Img(
+                                                    src='''https://placeholder.pics/svg/300x300/1797FF-773BFF''',
+                                                    alt='',
+                                                ), 
+                                                Img(
+                                                    src='''https://placeholder.pics/svg/300x300/1797FF-773BFF''',
+                                                    alt='',
+                                                ), )
+                                        ), 
+                                        P(
+
+                                            data = ('''Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse beatae aspernatur tempore similique enim, doloribus et ullam quam itaque sit! Molestias, perspiciatis omnis illo aliquam dignissimos laborum iste? Autem, esse?''',)
+                                        ), )
+                                ), 
+                                Div(
+                                    classs='''columns shrink footer text-center''',
+                                    data = (
+                                        P(
+
+                                            data = ('''Footer''',)
+                                        ), 
+                                        Ul(
+                                            classs='''menu align-center''',
+                                            data = (
+                                                Li(
+
+                                                    data = (
+                                                        A(
+                                                            href='',
+                                                            data = ('''Link''',)
+                                                        ), )
+                                                ), 
+                                                Li(
+
+                                                    data = (
+                                                        A(
+                                                            href='',
+                                                            data = ('''Link''',)
+                                                        ), )
+                                                ), 
+                                                Li(
+
+                                                    data = (
+                                                        A(
+                                                            href='',
+                                                            data = ('''Link''',)
+                                                        ), )
+                                                ), )
+                                        ), )
+                                ), )
+                        ), )
+                ), )
+        ), )
+)
