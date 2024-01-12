351382e917470a2e40468d9c998b302f08f208ed
Mohammadreza Amani <More.amani@yahoo.com>
2024-01-12 12:35:35 +0330

    Update version to 0.4.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 
-VERSION = "0.4.1-dev"
+VERSION = "0.4.2"
 
 PACKAGE_NAME = "inui"
 DESCRIPTION = ""
