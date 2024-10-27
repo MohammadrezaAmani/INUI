814d4cbc241d6b3d9333c60d8eca4ea29ace032e
Mohammadreza Amani <More.Amani@yahoo.com>
2024-10-28 03:21:32 +0330

    feat(tailwind): add tailwindcss support to project.

--- inui/conf.py
+++ inui/conf.py
@@ -8,3 +8,9 @@ BOOTSTRAP_JS_URL = (
     "https://cdn.jsdelivr.net/npm/bootstrap@%s/dist/js/bootstrap.bundle.min.js"
     % BOOTSTRAP_VERSION
 )
+
+TAILWINDCSS_URL = "https://cdn.tailwindcss.com"
+
+TAILWINDCSS_CONFIG = {}
+
+TAILWINDCSS_CUSTOM_CSS = ""
---
+++ inui/tailwind/utils.py
@@ -0,0 +1,9 @@
+def tw_class_to_lower(name: str):
+    name = name.replace("_", "-")
+    return name
+
+
+def tw_lower_to_class(name: str):
+    name = name.replace(".", "")
+    name = name.replace("-", "_")
+    return name
