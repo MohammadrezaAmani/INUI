69da2eaa5213b849087a741ab0a8668bb93fdea1
Mohammadreza Amani <more.amani@yahoo.com>
2024-11-09 17:21:18 +0330

    upgrade(v-0.9.6): ease usage of inui.

--- inui/base.py
+++ inui/base.py
@@ -9,17 +9,21 @@ class Attributes:
     def __init__(self, **kwargs: Any) -> None:
         self.attrs: Dict[str, Any] = {key: value for key, value in kwargs.items()}
 
-    def __getattribute__(self, __name: str) -> Any:
-        if __name == "attrs":
-            return super().__getattribute__("attrs")
-        return self.attrs.get(__name, None)
+    # def __getattribute__(self, __name: str) -> Any:
+    #     if __name == "attrs":
+    #         return super().__getattribute__("attrs")
+    #     return self.attrs.get(__name, None)
 
     def __str__(self) -> str:
         return " ".join(
-            f'{replace(name)}="{value}"'
+            f'{replace(name)}="{self._str(value)}"'
             for name, value in self.attrs.items()
             if value is not None
         )
+
+    def fix_values(self, value: str | list = ""):
+        if isinstance(value, str):
+            return value
 
     def __getitem__(self, index: str) -> Any:
         return self.attrs.get(index, None)
@@ -51,9 +55,6 @@ class Attributes:
     def update(self, **kwargs: Any) -> None:
         self.attrs.update(kwargs)
 
-    def copy(self) -> Attributes:
-        return Attributes(**self.attrs)
-
     def clear(self) -> None:
         self.attrs.clear()
 
@@ -62,6 +63,31 @@ class Attributes:
 
     def popitem(self) -> Tuple[str, Any]:
         return self.attrs.popitem()
+
+    def append(self, key: str = "class", *values):
+        if key not in self.attrs.keys():
+            self.attrs[key] = values
+        else:
+            if isinstance(self.attrs[key], (list, tuple, set)):
+                self.attrs[key] = (*self.attrs[key], *values)
+            else:
+                self.attrs[key] = (self.attrs[key], *values)
+
+    def _str(self, data, seperator=" ", fill=""):
+        try:
+            if isinstance(data, type):
+                if isinstance(data(), Base):
+                    data = data()
+        except Exception as _:
+            pass
+
+        if isinstance(data, (set, tuple, list)):
+            result = seperator.join([str(i) for i in data])
+            return f"{fill}{result}{fill}"
+        return str(data)
+
+    def copy(self):
+        return self.attrs.copy()
 
 
 class _Data:
@@ -80,18 +106,36 @@ class _Data:
             pass
         return str(data)
 
+    def append(self, *args):
+        self._data = (*self._data, *args)
+
+    def copy(self):
+        return self._data.copy()
+
 
 class Meta(NamedTuple):
     start_tag: str = None
     end_tag: str = None
+    start_sign: str = "<"
+    end_sign: str = ">"
+    close_sign: str = "</"
+    void_sign: str = "/>"
+
+
+empty = Meta("", "", "", "", "", "")
 
 
 class Base:
     Meta: Type[Meta] = Meta()
+    _data = _Data()
+    _attributes = Attributes()
 
-    def __init__(self, *data: Any, **attributes: Any) -> None:
+    def __init__(
+        self, *data: Any, _meta: Type[Meta] | None = None, **attributes: Any
+    ) -> None:
         self.data = data
         self.attributes = attributes
+        self.Meta = _meta or self.Meta
 
     @property
     def data(self) -> list:
@@ -105,7 +149,7 @@ class Base:
     def start_tag(self) -> str:
         return (
             self.Meta.start_tag
-            if self.Meta.start_tag
+            if self.Meta.start_tag is not None
             else self.__class__.__name__.lower()
         )
 
@@ -116,7 +160,9 @@ class Base:
     @property
     def end_tag(self) -> str:
         return (
-            self.Meta.end_tag if self.Meta.end_tag else self.__class__.__name__.lower()
+            self.Meta.end_tag
+            if self.Meta.end_tag is not None
+            else self.__class__.__name__.lower()
         )
 
     @end_tag.setter
@@ -136,7 +182,9 @@ class Base:
         )
 
     def render(self, prettify: bool = False) -> str:
-        return f"<{self.start_tag}{' ' if len(self.attributes) > 0 else ''}{str(self.attributes)}>{str(self.data)}</{self.end_tag}>"
+
+        return f"{self.Meta.start_sign}{self.start_tag}{' ' if len(self.attributes) > 0 else ''}\
+{str(self.attributes)}{self.Meta.end_sign}{str(self.data)}{self.Meta.close_sign}{self.end_tag}{self.Meta.end_sign}"
 
     def __str__(self) -> str:
         return self.render()
@@ -145,7 +193,7 @@ class Base:
         return self.render()
 
     def __add__(self, other) -> str:
-        return self.__str__() + " " + self._str(other)
+        return Base(self.__str__() + " " + self._str(other), _meta=empty)
 
     def __radd__(self, other) -> str:
         return self._str(other) + " " + self.__str__()
@@ -166,19 +214,84 @@ class Base:
         return str(data)
 
     def __lt__(self, other):
-        return f"{self._str(other)} > {self.__str__()}"
+        return Base(f"{self._str(other)} > {self.__str__()}", _meta=empty)
 
     def __gt__(self, other):
-        return f"{self.__str__()} > {self._str(other)}"
+        return Base(f"{self.__str__()} > {self._str(other)}", _meta=empty)
 
     def __le__(self, other):
-        return f"{self.__str__()} <= {self._str(other)}"
+        return Base(f"{self.__str__()} <= {self._str(other)}", _meta=empty)
 
     def __ge__(self, other):
-        return f"{self.__str__()} >= {self._str(other)}"
+        return Base(f"{self.__str__()} >= {self._str(other)}", _meta=empty)
+
+    def __mul__(self, value: int) -> str:
+        return Base(self.__str__() * int(value), _meta=empty)
+
+    def __rmul__(self, value: int) -> str:
+        return self.__mul__(value)
+
+    def __imul__(self, value: int) -> str:
+        return self.__mul__(value)
 
     def css(self, tag: str = ""):
         return tag + self.start_tag
+
+    def __matmul__(self, other):
+        return self._temp(other)
+
+        # return self._data.append(other)
+
+    def __and__(self, other):
+        return self._temp(other)
+
+    def __or__(self, other):
+        return self._temp(other)
+
+    def __mod__(self, other):
+        return self._temp(other)
+
+    def __truediv__(self, other):
+        if not isinstance(other, (list, set, tuple)):
+            other = (other,)
+        return self._attributes.append(*other)
+
+    def __imatmul__(self, other):
+        return self._itemp(other)
+
+    def __iand__(self, other):
+        return self._itemp(other)
+
+    def __ior__(self, other):
+        return self._itemp(other)
+
+    def __imod__(self, other):
+        return self._itemp(other)
+
+    def __itruediv__(self, other):
+        return self._itemp(other)
+
+    def copy(self):
+        return self.__class__(*self._data._data, **self._attributes.copy())
+
+    def _temp(self, other):
+        if not isinstance(other, (list, set, tuple)):
+            other = (other,)
+        copy = self.copy()
+        copy._attributes.append(
+            "class",
+            *other,
+        )
+        return copy
+
+    def _itemp(self, other):
+        if not isinstance(other, (list, set, tuple)):
+            other = (other,)
+        self._attributes.append(
+            "class",
+            *other,
+        )
+        return self
 
 
 class BaseElement(Base): ...
@@ -186,7 +299,7 @@ class BaseElement(Base): ...
 
 class BaseVoidElement(BaseElement):
     def render(self, prettify: bool = False):
-        return f"""<{self.start_tag}{' ' if len(self.attributes) > 0 else ''}{str(self.attributes)}> """
+        return f"""{self.Meta.start_sign}{self.start_tag}{' ' if len(self.attributes) > 0 else ''}{str(self.attributes)}{self.Meta.close_sign} """
 
 
 class BaseSvgElement(Base): ...
--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-from inui.css import CSS, align_content, all, margin
-from inui.css.size import Px
+from inui.elements import H1, H2
 
-print({margin(): margin.auto() + Px(1) + Px(2)})
-
-print(str("salam" > all.inherit()))
-
-print(
-    CSS(
-        {".h1": align_content + Px(1)},
-        {".h1": align_content + Px(1)},
-        {".h1": align_content.center + Px(1)},
-        h2=align_content + Px(1),
-    )
-)
+print(H1("salam") + H2("chetory?") * 100 + "Mohammadreza")
